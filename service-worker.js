df0f4508e24e914f940d87377a4a291e6317ceb0
EKEKWE-DIVINE <divineekekwecm@gmail.com>
2025-04-01 02:17:08 +0100

    first commit

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+
+const CACHE_NAME = "todo-app-cache-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./assets/pic/appicon.png",
+    "./manifest.json",
+    "https://fonts.googleapis.com/icon?family=Material+Icons"
+ ];
+
+
+ self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+       .then(cache => {
+         console.log("Caching files.....")
+         return cache.addAll(urlsToCache);
+       })
+    )
+ })
+
+ self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).then(fetchRes => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, fetchRes.clone());
+                    return fetchRes;
+                });
+            });
+        }).catch(() => caches.match("./index.html")) // Fallback for offline
+    );
+});
+
+self.addEventListener("activate" , event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== CACHE_NAME){
+                        console.log("Deleting old cache....");
+                        return caches.delete(cache);
+                        
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener("push" , event => {
+    const options = {
+        body: "New Todo Added",
+        icon: "./assets/pic/to-do.png",
+        badge: "./assets/pic/to-do.png",
+        vibrate: [200 , 100 ,200]
+    }
+    event.waitUntil(
+        self.registration.showNotification("To-Do App", options)
+    )
+})
